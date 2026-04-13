9da1ea7303a63996cbacc65a1437782e734ece2c
Renan <140409792+Renanstx@users.noreply.github.com>
2026-04-12 21:40:08 -0300

    Add basic calculator functions in script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    const input = document.getElementById('result').value;
+    try {
+        const result = eval(input);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Erro';
+    }
+}
+function deleteLast() {
+    const currentInput = document.getElementById('result').value;
+    document.getElementById('result').value = currentInput.slice(0, -1); // Remove o último caractere
+}
